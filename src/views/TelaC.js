426ac1bc47c482c34270df2fb066fb9fde2d4781
Leticia Rodrigues <leticiaarodrigues2@gmail.com>
2021-12-03 23:10:42 -0300

    Preparando App aula 84 e 85

---
+++ src/views/TelaC.js
@@ -0,0 +1,8 @@
+import React from "react";
+import TextoCentral from "../components/TextoCentral";
+
+export default props => (
+  <TextoCentral corFundo=  '#7b14e7'>
+    Tela C
+  </TextoCentral>
+)
