048e855dbbb90a2d8fe909658853592527e033ff
Leticia Rodrigues <leticiaarodrigues2@gmail.com>
2021-12-11 15:36:01 -0300

    Tabs aula 92

--- src/views/TelaC.js
+++ src/views/TelaC.js
@@ -2,9 +2,10 @@ import React from "react";
 import TextoCentral from "../components/TextoCentral";
 
 export default props =>{
-  const route = props.route || {params : {numero: 0}}
+  const r = props.route
+  const numero = r && r.params && r.params.numero ? props.route.params.numero : 0
 return (
   <TextoCentral corFundo=  '#7b14e7'>
-    Tela C - {route.params.numero}
+    Tela C - {numero}
   </TextoCentral>
 )}
